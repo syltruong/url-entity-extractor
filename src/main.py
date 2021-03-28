beb590ce10cb98455a57328312d4e59552109d90
syltruong <syltruong@gmail.com>
2021-03-28 11:55:30 +0000

    rm old endpoints

--- src/main.py
+++ src/main.py
@@ -14,16 +14,6 @@ class Source(BaseModel):
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
-
-
 @app.post("/extract")
 def extract(source: Source):
 
